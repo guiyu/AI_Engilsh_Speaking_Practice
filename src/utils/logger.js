d6b21c8cfef51a71830210ad67e92b1dacee5406
weiyi <weiyi415@gmail.com>
2025-01-03 11:38:32 +0800

    update debug settings

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,28 +1,37 @@
 // 创建 logger.js 在 utils 目录下
 import { DebugPanel } from './debugPanel.js';
+const IS_DEVELOPMENT = false; // 在这里控制是否为开发环境
 
 export class Logger {
     static log(...args) {
-        const debugPanel = DebugPanel.getInstance();
-        debugPanel.log('[DEBUG]', ...args);
-        console.debug(...args);
+        if (IS_DEVELOPMENT) {
+            const debugPanel = DebugPanel.getInstance();
+            debugPanel.log('[DEBUG]', ...args);
+            console.debug(...args);
+        }
     }
 
     static info(...args) {
-        const debugPanel = DebugPanel.getInstance();
-        debugPanel.log('[INFO]', ...args);
-        console.info(...args);
+        if (IS_DEVELOPMENT) {
+            const debugPanel = DebugPanel.getInstance();
+            debugPanel.log('[INFO]', ...args);
+            console.info(...args);
+        }
     }
 
     static warn(...args) {
-        const debugPanel = DebugPanel.getInstance();
-        debugPanel.log('[WARN]', ...args);
-        console.warn(...args);
+        if (IS_DEVELOPMENT) {
+            const debugPanel = DebugPanel.getInstance();
+            debugPanel.log('[WARN]', ...args);
+            console.warn(...args);
+        }
     }
 
     static error(...args) {
-        const debugPanel = DebugPanel.getInstance();
-        debugPanel.log('[ERROR]', ...args);
-        console.error(...args);
+        if (IS_DEVELOPMENT) {
+            const debugPanel = DebugPanel.getInstance();
+            debugPanel.log('[ERROR]', ...args);
+            console.error(...args);
+        }
     }
 }
